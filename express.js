bdb353662db0cb5bfab20c88b7af543e9e6fbdc9
Guddu Kumar <73424882+guddu1cse@users.noreply.github.com>
2025-01-24 12:17:53 +0530

    update

--- express.js
+++ express.js
@@ -1,15 +1,17 @@
-
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/home/:message/:now' , (req , res)=>{
+app.post('/home/:message/:now' , (req , res)=>{
     const prams = req.params
     const requestParam = req.query;
+    const reqData = req.body || {Body :[req.body]};
+    console.log(reqData);
     console.log(requestParam);
     console.log(prams);
-    res.end(`path variables => ${Object.values(prams)} \n request prams => ${Object.values(requestParam)}`);
+    res.end(`path variables => ${Object.values(prams)} \nrequest prams => ${Object.values(requestParam)} \n Body => ${Object.entries(reqData)}`);
 });
 
 const PORT = process.env.PORT || 4041;
